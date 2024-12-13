023241743abad5bf0b53731dc722c8d8ee9e91a2
David Alvarez <david.a@casautomotive.com>
2024-12-13 14:51:11 -0500

    turn around

--- handlers/website.go
+++ handlers/website.go
@@ -17,8 +17,8 @@ import (
 )
 
 const (
-	YovaHeroImage string = "https://ydcocktails.s3.us-east-1.amazonaws.com/media/yova_mid_cta.jpg"
-	YovaMidCTA    string = "https://ydcocktails.s3.us-east-1.amazonaws.com/media/yova_hero.jpeg"
+	YovaHeroImage string = "https://ydcocktails.s3.us-east-1.amazonaws.com/media/yova_hero.jpeg"
+	YovaMidCTA    string = "https://ydcocktails.s3.us-east-1.amazonaws.com/media/yova_mid_cta.jpg"
 )
 
 var decoder = schema.NewDecoder()
