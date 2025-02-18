007670244aa825c7757b9f2a097fca078c602004
David Alvarez <david.a@casautomotive.com>
2025-02-18 10:34:54 -0500

    lead checker

--- services/sms.go
+++ services/sms.go
@@ -19,7 +19,16 @@ func checkSMS() {
 		subject := fmt.Sprintf("%d UNREAD MESSAGES", unreadMessages)
 		recipients := []string{constants.CompanyEmail}
 
-		body := fmt.Sprintf("Content-Type: text/html; charset=UTF-8\r\n%s", "LINK: "+fmt.Sprintf("%s/crm/message", constants.RootDomain))
+		body := fmt.Sprintf("Content-Type: text/html; charset=UTF-8\r\n%s", fmt.Sprintf(`
+		<!DOCTYPE html>	
+		<html>
+				<head>Unread Messages E-mail Notification</head>
+				<body>
+					<p>You have <strong>%d</strong> unread messages in the last 5 minutes.</p>
+					<p><a href="%s/crm/message">Click here to view your messages</a></p>
+				</body>
+			</html>`, unreadMessages, constants.RootDomain))
+
 		err = SendGmail(recipients, subject, constants.CompanyEmail, body)
 		if err != nil {
 			fmt.Printf("ERROR SENDING UNREAD MESSAGES NOTIFICATION EMAIL: %+v\n", err)
