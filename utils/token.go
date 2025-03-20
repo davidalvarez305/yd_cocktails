e2cbd261ded00e363317eae6f732941d3f8326b2
David Alvarez <david.a@casautomotive.com>
2025-03-20 14:31:28 -0400

    local date

--- utils/token.go
+++ utils/token.go
@@ -14,19 +14,19 @@ func FormatTimestampEST(timestamp int64) string {
 }
 
 func FormatTimestamp(timestamp int64) string {
-	t := time.Unix(timestamp, 0).UTC()
+	t := time.Unix(timestamp, 0)
 	formattedTime := t.Format("01/02/2006 03:04:05 PM")
 	return formattedTime
 }
 
 func FormatDateMMDDYYYY(timestamp int64) string {
-	t := time.Unix(timestamp, 0).UTC()
+	t := time.Unix(timestamp, 0)
 	formattedTime := t.Format("01/02/2006")
 	return formattedTime
 }
 
 func FormatDateJanDDYYYY(timestamp int64) string {
-	t := time.Unix(timestamp, 0).UTC()
+	t := time.Unix(timestamp, 0)
 	formattedTime := t.Format("Jan 2, 2006")
 	return formattedTime
 }
